05f03c22775879cd2d6869b849a08a7524e77b88
lewyonq <jakub.lewandowski98@gmail.com>
2025-02-24 20:26:36 +0100

    init

---
+++ src/main/java/com/avocados/comdash/ComdashApplication.java
@@ -0,0 +1,13 @@
+package com.avocados.comdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComdashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComdashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avocados/comdash/ComdashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocados.comdash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComdashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
