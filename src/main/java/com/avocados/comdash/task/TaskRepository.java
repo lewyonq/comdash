57f29e409825e4aa7c29db7d57c0557a7473353a
nokciec <dominika.poreba@gmail.com>
2025-02-25 20:06:32 +0100

    modify repository

--- src/main/java/com/avocados/comdash/task/TaskRepository.java
+++ src/main/java/com/avocados/comdash/task/TaskRepository.java
@@ -1,7 +1,8 @@
 package com.avocados.comdash.task;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TaskRepository {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
+
